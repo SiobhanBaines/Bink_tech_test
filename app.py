8eef62f355a058e40bfe3aab088bceb02935b2e5
SiobhanBaines <siobhan.baines@gmail.com>
2021-08-22 14:14:13 +0000

    Add sort functionality

--- app.py
+++ app.py
@@ -7,32 +7,59 @@ def load_data():
     """
     file = open('mobile-phone-masts-uk.csv')
     reader = csv.reader(file)
-
     header = next(reader)
+    print(header)
 
     mast_data =[]
 
     for row in reader:
-        property_name = row[0],
-        property_address_1 = row[1],
-        property_address_2 = row[2],
-        property_address_3 = row[3],  
-        property_address_4 = row[4],
-        unit_name = row[5],
-        tenant_name = row[6],
-        lease_start_date = datetime.strptime(row[7], '%d %b %Y'), 
-        lease_end_date = datetime.strptime(row[8], '%d %b %Y'),
-        lease_years = int(row[9]),
-        current_rent = float(row[10])
+        print(row)
+        mast_data.append([
+        row[0],
+        row[1],
+        row[2],
+        row[3],  
+        row[4],
+        row[5],
+        row[6],
+        datetime.strptime(row[7], '%d %b %Y'), 
+        datetime.strptime(row[8], '%d %b %Y'),
+        int(row[9]),
+        float(row[10])])
         
-        mast_data.append([property_name, property_address_1, property_address_3, property_address_4, unit_name, tenant_name, lease_start_date, lease_end_date, lease_years, current_rent])
+        # mast_data.append()
+        print(mast_data)
 
-    return(mast_data)
+    print(mast_data)
+    return(mast_data, header)
+
+def sort_data(data, header):
+    print('The field headings are:')
+
+    for h in header:
+        print(h)
+        
+    sort_by = input("Enter the heading you wish to sort by here: ")
+    
+    i = header.index(sort_by)
+    data.sort(key=lambda x:x[i])
+    return data
+
+def get_sort_criteria(header):
+    
+    
+    return header(sort_by)
 
 def main():
-    print('line 34')
-    mast_data_list = load_data()
-    print("This is the data from the csv file:")
+    
+    mast_data_list, headings = load_data()
+
+    print("This is the first 5 items from the csv file after loading into a list:")
     print(mast_data_list[:5])
+    
+    sorted_list = sort_data(mast_data_list, headings)
+    print("This are the first 5 items from the sorted list:")
+    print(sorted_list[:5])
+
 
 main()
